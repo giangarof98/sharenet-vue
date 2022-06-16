91c58a619fa387ca5d06a06e550f42b0e1f33c6c
Gianmarco G <giangarof98@gmail.com>
2022-06-16 15:55:20 -0400

    added express-session and user model

--- back/index.js
+++ back/index.js
@@ -2,13 +2,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const session = require('express-session');
+const passport = require('passport');
+const local = require('passport-local');
 
 //Port
 const port = process.env.PORT || 3000;
 
 //Models
-//const Content = require('./model/content');
+const User = require('./model/user')
 
 //Routes
 const routesContent = require('./routes/content')
@@ -31,6 +34,35 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(methodOverride('_method'));
 
+const sessionConfig = {
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    }
+}
+app.use(session(sessionConfig));
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new local(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Endpoint
 app.use('/', routesContent);
+
+//User
+app.get('/test', async (req,res) => {
+    const user = new User({email:'test@test.com', username:'testtt'})
+    const newU = await User.register(user, 'test');
+    res.send(newU)
+});
+
+
 
 app.listen(port, () => { console.log('connected!')});
