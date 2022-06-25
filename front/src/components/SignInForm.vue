42e200d65b3d4edd25695d2aa300cb68df350252
Gianmarco G <giangarof98@gmail.com>
2022-06-24 21:23:07 -0400

    signin template

--- front/src/components/SignInForm.vue
+++ front/src/components/SignInForm.vue
@@ -1,7 +1,16 @@
 <template>
-    <form action="">
-        SignIn
-    </form>
+    <div class="flex justify-center mt-10">
+        <form action="" class="flex flex-col justify-center w-72 p-4">
+            <label for="email" class="font-semibold text-lg">Email</label>
+            <input type="text" id="email" name="email" class="border rounded py-0.9 px-1 focus:outline-none">
+
+            <label for="password" class="font-semibold text-lg">Password</label>
+            <input type="text" id="password" name="password" class="border rounded py-0.9 px-1 focus:outline-none">
+
+            <button class="font-semibold text-lg bg-button rounded p-1 text-white mt-2">Sign In</button>
+        </form>
+
+    </div>
 </template>
 
 <script>
